f8a36a5d32a4b6f4de046544f9ae4bc2e954c7b1
Bence Ujj <ehog@ehog.hu>
2025-05-19 12:26:16 +0200

    V5.0.1 (#252)
    * 5.0.1
    
    * fix: Initial state for signals

--- src/monitor.signal.operator.ts
+++ src/monitor.signal.operator.ts
@@ -5,12 +5,14 @@ export function monitorSignal<T>(streamMonitor: MonitorSignal): OperatorFunction
   return (source$: Observable<T>): Observable<T> =>
     new Observable<T>((observer) => {
       streamMonitor.reset();
+      streamMonitor.setActive();
       const wrapper: PartialObserver<T> = {
         next: (value: T) => {
           streamMonitor.incrementPumps();
           observer.next(value);
         },
         error: (error) => {
+          streamMonitor.setInactive();
           streamMonitor.setError(error);
           observer.error(error);
         },
